307dcc6a084a882a24092b5f54c0c47b956a40c9
Jeremiahade <jeremiahadekoya@gmail.com>
2024-04-13 14:18:30 +0100

    Add webserver switch feature. Change and test deep sleep algortihm. Add WiFi reconnect feature

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,49 @@
 
 #include <gardenlighthw.h>
 #include <millisDelay.h>
+#include <WiFi.h>
 
+// TODO: Configure your WiFi here
+//#define WIFI_SSID "VM1628887"
+//#define WIFI_PSK  "jeojc7gwbnPkvzcg"
+#define WIFI_SSID "Freeloadersunited"
+#define WIFI_PSK  "klak8385"
+
+/** Check if we have multiple cores */
+#if CONFIG_FREERTOS_UNICORE
+#define TASK_CORE 0
+#else
+#define TASK_CORE 1
+#endif
+
+#if 0
+// Include certificate data (see note above)
+#include "cert.h"
+#include "private_key.h"
+
+// We will use wifi
+
+
+// Includes for the server
+#include <HTTPSServer.h>
+#include <SSLCert.h>
+#include <HTTPRequest.h>
+#include <HTTPResponse.h>
+
+// The HTTPS Server comes in a separate namespace. For easier use, include it here.
+using namespace httpsserver;
+
+#endif
 
 #define DEFAULT_DELAY_TIME      10000 // 10 seconds
-#define DUMMY_LIGHT_SENSE       0
+#define DUMMY_LIGHT_SENSE       1
 #define SWITCH_DEBOUNCE_DELAY   50    // 50 mS
+#define LIGHT_DEBOUNCE_DELAY    100   // 100 mS
 #define PIR_SENSE_DEBOUNCE      25    //25 mS
-#define LOOP_DEBUG_DELAY        3000 // 6 Seconds
+#define LOOP_DEBUG_DELAY        3000  // 6 Seconds
+#define TEST_MANUAL_DELAY       1     // is high when test manual state is active
+
+#define SIMPLE_HTTP_SERVER      1   //simple http server active if 1
 
 // put function declarations here:
   FloodLight gardenLight("gardenlight", GARDEN_LIGHT);
@@ -38,12 +74,16 @@
   millisDelay mockSenseDebounce;
   millisDelay manualtestDebounce;
   millisDelay keypadDebounce;
+  millisDelay lightSenseDebounce;
 
 // Declare state variables and flags
   uint8_t currManualSwState = 0;
-  uint8_t manualSwReading = 0;
   uint8_t manualTimeoutFlag = 0;
-  uint16_t manualDelayTime = 3000;
+  #if TEST_MANUAL_DELAY
+    uint32_t manualDelayTime = 3000;    // 3 seconds delay
+  #else
+    uint32_t manualDelayTime = 300000;  // 5 minutes delay
+  #endif
   uint8_t manualActiveFlag = 0;
 
   uint8_t currManualTestState = 1;
@@ -56,6 +96,7 @@
   uint8_t lastPireading = 0;
   uint8_t motionLightActiveFlag = 0;
   uint8_t timeDelayStartFlag = 0;
+  uint16_t motionTimeDelay = 20000; // 20 seconds default time delay
 
   uint8_t currKeypadState = 0;
   uint8_t keypadReading = 0;
@@ -65,7 +106,27 @@
   uint8_t mockSenseReading = 0;
   uint8_t lastMockSenseReading = 0;
 
+  uint8_t lightSenseState = 1;
+  uint8_t lightSenseReading = 0;
+  uint8_t lastLightSenseReading = 0;
+
   uint16_t goToSleep __section(".noinit");
+
+// Wifi and webserver variables
+// Set web server port number to 80
+  WiFiServer server(80);
+  unsigned long previousReconMillis = 0;
+  unsigned long currReconMillis = 0;
+  unsigned long reconDelay = 10000;
+
+// Variable to store the HTTP request
+  String header;
+// Current time
+  unsigned long currentTime = millis();
+// Previous time
+  unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+  const long timeoutTime = 2000;
 
 // State Logic functions definitons:
   /**************************************************************************/
@@ -98,16 +159,22 @@
 */
 /**************************************************************************/
   void KeypadLogic(void);
-
-  
+  /**************************************************************************/
+/*!
+    @brief This is the web server FreeRTOS task function.  
+*/
+/**************************************************************************/
+void WebServerTask(void* params);  
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  delay(4000);
   #if DEBUG
   delay(10000);
   #endif
-  while(esp_sleep_enable_ext0_wakeup(GPIO_NUM_26, 0) != ESP_OK);
+  while(esp_sleep_enable_ext0_wakeup(GPIO_NUM_26, 1) != ESP_OK);
+
   Serial.println("Welcome, Starting Garden Light Control!!!");
   manualModeLamp.Activate();
   wifiConnectLamp.Activate();
@@ -116,6 +183,8 @@ void setup() {
   remoteSenseLamp.Activate();
   #if DEBUG
   delay(10000);
+  #else
+  delay(1000);
   #endif
    manualModeLamp.Deactivate();
   wifiConnectLamp.Deactivate();
@@ -127,6 +196,8 @@ void setup() {
   currManualTestState =manualTest.GetState();
   currMockSenseState = mockSensor.GetSignal();
   currPirSenseState = pirSensor1.GetSignal();
+
+  TaskHandle_t wifiTaskHandle = NULL;
  
  // If go to sleep is set, start deep sleep
   if(goToSleep == 0xdd11)
@@ -135,15 +206,79 @@ void setup() {
     Serial.println("Going to sleep");
     esp_deep_sleep_start();
   }
+  lightSenseState = lightSensor.GetSignal();
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+  #if !DEBUG
+    WiFi.begin(WIFI_SSID, WIFI_PSK);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    // Print local IP address and start web server
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    server.begin();
+    xTaskCreatePinnedToCore(WebServerTask, "http80", 3000, NULL, 1, &wifiTaskHandle, TASK_CORE);
+   #endif
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(lightSensor.GetSignal() && DUMMY_LIGHT_SENSE)
+  //Serial.println("in the loop");
+  lightSenseReading = lightSensor.GetSignal();
+  
+  #if DEBUG
+    if(lightSenseReading)
+    {
+      Serial.println("light sense reading is HIGH");
+    }
+    else{
+      Serial.println("light sense reading is LOW");
+    }
+    delay(LOOP_DEBUG_DELAY);
+  #endif
+  // Debounce the Light Sensor input
+  // light sensor output is 0(LOW) when no light is detected
+  // and 1(HIGH) when light is detected
+    if(lastLightSenseReading != lightSenseReading)
+    {
+      lightSenseDebounce.start(LIGHT_DEBOUNCE_DELAY);
+      #if DEBUG
+        Serial.println("debounce started");
+        delay(LOOP_DEBUG_DELAY);
+      #endif
+    }
+    if(lightSenseDebounce.justFinished())
+    {
+      if(lightSenseReading != lightSenseState)
+      {
+        lightSenseState = lightSenseReading;
+        #if DEBUG
+          if(lightSenseState)
+          {
+            Serial.println("light sense state is HIGH");
+          }
+          else{
+            Serial.println("light sense state is LOW");
+          }
+          delay(LOOP_DEBUG_DELAY);
+        #endif
+      }
+      lightSenseDebounce.finish();
+    }
+  #if DEBUG
+    Serial.println("light sense??");
+  #endif
+  if(!(lightSenseState) && DUMMY_LIGHT_SENSE)
   {
-    //goToSleep = 0xdd11;
+    goToSleep = 0xdd11;
     Serial.println("Sleep mode activated.. restarting system");
-    //ESP.restart();
+    ESP.restart();
   }
   else
   {
@@ -182,8 +317,9 @@ void loop() {
     lastMockSenseReading = mockSenseReading;
     lastPireading = pirReading;
     lastKeypadReading = keypadReading;
+    
   }
-
+  lastLightSenseReading = lightSenseReading;
 }
 
 /**************************************************************************/
@@ -312,7 +448,7 @@ void MotionStateLogic(void)
     {
       if(!timeDelayStartFlag)
       {
-        timeDelay.start(20000);
+        timeDelay.start(motionTimeDelay);
         timeDelayStartFlag =1;
       }
       //when countdown timer is expired, turn off
@@ -427,3 +563,123 @@ void KeypadLogic(void)
     currMockSenseState = 1;
   }
 }
+
+#if SIMPLE_HTTP_SERVER
+void WebServerTask(void* params)
+{
+  for(;;)
+  {
+    //Serial.println("in the core 1");
+    if(WiFi.status() != WL_CONNECTED)
+    {
+      wifiConnectLamp.Deactivate();
+      Serial.println("wifi is disconnected");
+    }
+    else{
+      wifiConnectLamp.Activate();
+    }
+    if((WiFi.status() != WL_CONNECTED) && (currReconMillis - previousReconMillis >= reconDelay))
+    {
+      WiFi.disconnect();
+      WiFi.reconnect();
+      previousReconMillis = currReconMillis;
+    }
+
+    if(WiFi.status() == WL_CONNECTED)
+    {
+      WiFiClient client = server.available();   // Listen for incoming clients
+
+      if (client) {                             // If a new client connects,
+        currentTime = millis();
+        previousTime = currentTime;
+        Serial.println("New Client.");          // print a message out in the serial port
+        String currentLine = "";                // make a String to hold incoming data from the client
+        while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+          currentTime = millis();
+          if (client.available()) {             // if there's bytes to read from the client,
+            char c = client.read();             // read a byte, then
+            Serial.write(c);                    // print it out the serial monitor
+            header += c;
+            if (c == '\n') {                    // if the byte is a newline character
+              // if the current line is blank, you got two newline characters in a row.
+              // that's the end of the client HTTP request, so send a response:
+              if (currentLine.length() == 0) {
+                // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                // and a content-type so the client knows what's coming, then a blank line:
+                client.println("HTTP/1.1 200 OK");
+                client.println("Content-type:text/html");
+                client.println("Connection: close");
+                client.println();
+                
+                // turns the GPIOs on and off
+                if (header.indexOf("GET /26/on") >= 0) {
+                  currManualSwState = 1;
+                  
+                } else if (header.indexOf("GET /26/off") >= 0) {
+                  currManualSwState = 0;
+                  
+                }
+                
+                // Display the HTML web page
+                client.println("<!DOCTYPE html><html>");
+                client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                client.println("<link rel=\"icon\" href=\"data:,\">");
+                // CSS to style the on/off buttons 
+                // Feel free to change the background-color and font-size attributes to fit your preferences
+                client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+                client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+                client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+                client.println(".button2 {background-color: #555555;}</style></head>");
+                
+                // Web Page Heading
+                client.println("<body><h1>Garden Light Control</h1>");
+                
+                // Display current state, and ON/OFF buttons
+                if(currManualSwState)
+                {
+                  client.println("<p>Switch - State ON </p>");
+                } 
+                else{
+                  client.println("<p>Switch - State OFF</p>");
+                }
+                
+                // If the output26State is off, it displays the ON button       
+                if (currManualSwState==0) {
+                  client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+                } else {
+                  client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
+                } 
+                  
+                client.println("</body></html>");
+                
+                // The HTTP response ends with another blank line
+                client.println();
+                // Break out of the while loop
+                break;
+              } else { // if you got a newline, then clear currentLine
+                currentLine = "";
+              }
+            } else if (c != '\r') {  // if you got anything else but a carriage return character,
+              currentLine += c;      // add it to the end of the currentLine
+            }
+          }
+        }
+        // Clear the header variable
+        header = "";
+        // Close the connection
+        client.stop();
+        Serial.println("Client disconnected.");
+        Serial.println("");
+      }
+    }
+  }
+}
+#else
+void WebServerTask(void* params)
+{
+  for(;;)
+  {
+
+  }
+}
+#endif
